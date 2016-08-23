beda2626ceb439b5a837cf13a54140e479984771
David Kim <innerb@gmail.com>
2016-08-22 19:07:17 -0700

    Fixed up bar chart in reports page

--- app/js/controllers/reports.js
+++ app/js/controllers/reports.js
@@ -1,11 +1,21 @@
-function ReportsCtrl() {
+function ReportsCtrl($scope) {
+  'ngInject';
 
   // ViewModel
   const vm = this;
 
   vm.title = 'ReportsCtrl';
-  vm.number = 12345;
   console.log('in ReportsCtrl');
+
+  $scope.tsv = [
+    'letter	frequency',
+    'A	.08167',
+    'B	.01492',
+    'C	.02782',
+    'D	.04253',
+    'E	.12702'
+  ].join('\n');
+
 }
 
 export default {
--- app/js/directives/chart.js
+++ app/js/directives/chart.js
@@ -2,58 +2,79 @@ import * as d3 from 'd3';
 
 function BarChart() {
 
-  const tsv = [
-    'letter	frequency',
-    'A	.08167',
-    'B	.01492',
-    'C	.02782'
-  ].join('\n');
-
   const margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    width = 800 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
   return {
     restrict: 'EA',
     scope: {
-      // title: '@',
-      // message: '@clickMessage'
+      tsv: '=',
+      yAxisLabel: '@'
     },
     link: (scope, element) => {
-      const data = d3.tsvParse(tsv, (d) => {
-        return {
-          letter: d.letter, freq: +d.frequency
-        };
-      });
-
-      const barWidth = width / data.length;
-
-      const chart = d3.select(element[0]).append('svg');
-      chart
-        .attr('width', width)
-        .attr('height', height);
-
-      const bar = chart.selectAll('g')
-        .data(data)
-        .enter().append('g')
-        .attr('transform', function(d, i) { return 'translate(' + i * barWidth + ',0)'; });
 
       const x = d3.scaleBand()
-        .domain(['A','B','C'])
-        .rangeRound([0,width]);
+        .rangeRound([0,width])
+        .paddingInner(.1)
+        .paddingOuter(.1);
 
       const y = d3.scaleLinear()
-        .domain([0,d3.max(data, function(d){return d.freq})])
         .range([height, 0]);
 
-      bar.append('rect')
-        .attr('y', function(d) { return y(d.freq); })
-        .attr('height', function(d) { return height - y(d.freq); })
-        .attr('width', barWidth - 1);
+      const xAxis = d3.axisBottom(x);
+      const yAxis = d3.axisLeft(y)
+        .ticks(10, '%');
 
-      // element.on('click', () => {
-      //   window.alert('Element clicked: ' + scope.message);
-      // });
+      const chart = d3.select(element[0])
+        .append('svg')
+        .attr('width', width + margin.left + margin.right)
+        .attr('height', height + margin.top + margin.bottom)
+        .append('g')
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      let data = d3.tsvParse(scope.tsv);
+      const kOrdinal = data.columns[0];
+      const kValue = data.columns[1];
+
+      data = data.map((d) => {
+        return {
+          [kOrdinal]: d[kOrdinal],
+          [kValue]: +d[kValue] // converting to number
+        }
+      });
+
+      x.domain(data.map((d) => d[kOrdinal]));
+      y.domain([0,d3.max(data, (d) => d[kValue])]);
+
+      chart.append('g')
+        .attr('class', 'x axis')
+        .attr('transform', 'translate(0,' + height + ')')
+        .call(xAxis);
+
+      const yGroup = chart.append("g")
+        .attr("class", "y axis");
+
+      yGroup
+        .append('text')
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text(scope.yAxisLabel);
+
+      yGroup
+        .call(yAxis);
+
+      chart.selectAll(".bar")
+        .data(data)
+        .enter().append("rect")
+        .attr("class", "bar")
+        .attr("x", function(d) { return x(d[kOrdinal]); })
+        .attr("y", function(d) { return y(d[kValue]); })
+        .attr("height", function(d) { return height - y(d[kValue]); })
+        .attr("width", x.bandwidth());
+
     }
   };
 }
