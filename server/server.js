defa7ce681494bf32faa29d72829c4ad3a43d19b
David Kim <innerb@gmail.com>
2016-08-25 14:08:21 -0700

    User ES6 and async file write in server.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
-var express = require('express');
-var bodyParser = require('body-parser'); // for request.body
-var fs = require('fs');
+'use strict';
+const express = require('express');
+const bodyParser = require('body-parser'); // for request.body
+const fs = require('fs');
 
-var staticPath = __dirname + '/../build';
+const staticPath = __dirname + '/../build';
 console.log('staticPath: ' + staticPath);
+const writeFile = 'users.log';
+console.log('writeFile: ' + writeFile);
 
-var appRouter = express.Router();
+
+const appRouter = express.Router();
 appRouter
   .use('/js', express.static(staticPath + '/js'))
   .use('/css', express.static(staticPath + '/css'))
@@ -22,10 +26,10 @@ appRouter
  /api/users/:user_id	PUT	Update a bear with new info.
  /api/users/:user_id	DELETE	Delete a bear.
  */
-var userRouter = express.Router();
+const userRouter = express.Router();
 
 function validateForm(d) {
-  var reEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  const reEmail = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if(!reEmail.test(d.email)){
     return {error: 'bad email format'};
   }
@@ -39,18 +43,22 @@ userRouter
   //.use(bodyParser.urlencoded({ extended: false }))
   .use(bodyParser.json())
   .post('/', function (req, res) {
-    var result = validateForm(req.body);
+    const result = validateForm(req.body);
     if(result.success){
-      var writeFile = 'users.log';
-      fs.writeFileSync(writeFile, JSON.stringify(req.body, null, 4));
-      res.json({user_id: 1234});
+      fs.writeFile(writeFile, JSON.stringify(req.body, null, 4), (err) =>{
+        if(err){
+          throw err;
+        }
+        res.json({user_id: 1234});
+      });
+
     } else {
-      res.json(JSON.stringify(result));
+      res.json(result);
     }
   });
 
-var port = process.env.PORT || 8080;
-var app = express(); // angular app
+const port = process.env.PORT || 8080;
+const app = express(); // angular app
 app
   .use('/', appRouter)
   .use('/api/users', userRouter)
